7181556e6b347eea405cdb4fed8916d58e75a778
Olga Zhukova <zhukova.o.m@yandex.ru>
2018-03-30 20:35:09 +0500

    Объединение двух соседних кусочков памяти в один, присоединение хвоста одного к соседнему свободному, рефакторинг

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-#include "my_allocator.h"
+#include "MyAllocator.h"
+
+struct littleTest {
+    int a;
+};
 
 struct test {
     int a;
@@ -10,16 +14,22 @@ struct bigTest {
     double b;
     int c;
 };
+struct middleTest {
+    int a;
+    double b;
+    int c;
+};
 struct veryBigTest {
     int a;
     double b;
     int c;
     double d;
+    int e;
 };
 
 
 int main() {
-    my_allocator alloc = my_allocator();
+    MyAllocator alloc = MyAllocator();
     vector<void *> pointers;
     for (int i = 0; i < 5; ++i) {
         pointers.push_back(alloc.alloc(sizeof(test)));
@@ -27,14 +37,42 @@ int main() {
     }
 
     auto last = pointers[1];
-        alloc.free(last);
-        alloc.Dump();
+    alloc.free(last);
+    alloc.Dump();
 
     last = pointers[2];
     alloc.free(last);
     alloc.Dump();
 
+    last = pointers[4];
+    alloc.free(last);
+    alloc.Dump();
+
     pointers.push_back(alloc.alloc(sizeof(veryBigTest)));
     alloc.Dump();
+/*
+    //вставить большой кусок, объединив два
+    pointers.push_back(alloc.alloc(sizeof(middleTest)));
+    alloc.Dump();
+    //вставить маленький кусочек, объединить остаток
+*/
+    pointers.push_back(alloc.alloc(sizeof(littleTest)));
+    alloc.Dump();
+
+
+    pointers.push_back(alloc.alloc(sizeof(littleTest)));
+    alloc.Dump();
+    pointers.push_back(alloc.alloc(sizeof(test)));
+    alloc.Dump();
+    pointers.push_back(alloc.alloc(sizeof(test)));
+    alloc.Dump();
+
+    // ошибка
+    /*
+    for (int i = 0; i < 5; ++i) {
+        pointers.push_back(alloc.alloc(sizeof(test)));
+        alloc.Dump();
+    }
+     */
 
 }
